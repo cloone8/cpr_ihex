76e00ab04c7d9abe07eae686006b15cb78587666
Wouter de Bruijn <wouter@wutru.io>
2024-03-23 13:58:34 +0100

    Other records and debug optimizations

--- src/gui/main_panel.rs
+++ src/gui/main_panel.rs
@@ -10,28 +10,53 @@ use crate::ihex_record::{
 
 use super::Gui;
 
-fn display_data(_record: &DataRecord, ui: &mut Ui) {
-    ui.label("Data {NOT IMPLEMENTED YET}");
+fn numvec_as_hex_string(data: &[u8]) -> String {
+    let str = data
+        .iter()
+        .map(|byte| format!("0x{:02x}", byte))
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    format!("[{}]", str)
+}
+
+fn display_data(record: &DataRecord, ui: &mut Ui) {
+    ui.label(format!(
+        "Data: Address: 0x{:x} - Length: {}",
+        record.naive_address,
+        record.data.len()
+    ));
+    ui.label(format!("Data: {}", numvec_as_hex_string(&record.data)));
 }
 
 fn display_end_of_file(ui: &mut Ui) {
     ui.label("End of file");
 }
 
-fn display_extended_segment_address(_record: &ExtendedSegmentAddressRecord, ui: &mut Ui) {
-    ui.label("Extended segment address {NOT IMPLEMENTED YET}");
+fn display_extended_segment_address(record: &ExtendedSegmentAddressRecord, ui: &mut Ui) {
+    ui.label(format!(
+        "Extended segment address: Segment Base Address: {:x}",
+        record.segment_base
+    ));
 }
 
 fn display_start_segment_address(record: &StartSegmentAddressRecord, ui: &mut Ui) {
-    ui.label(format!("Start segment address: Code Segment: 0x{:x} - Instruction Pointer: 0x{:x}", record.code_segment, record.instruction_pointer));
+    ui.label(format!(
+        "Start segment address: Code Segment: 0x{:x} - Instruction Pointer: 0x{:x}",
+        record.code_segment, record.instruction_pointer
+    ));
 }
 
-fn display_extended_linear_address(_record: &ExtendedLinearAddressRecord, ui: &mut Ui) {
-    ui.label("Extended linear address {NOT IMPLEMENTED YET}");
+fn display_extended_linear_address(record: &ExtendedLinearAddressRecord, ui: &mut Ui) {
+    let base_32: u32 = (record.address_base as u32) << 16;
+    ui.label(format!(
+        "Extended linear address: Linear Base Address: 0x{:x}",
+        base_32
+    ));
 }
 
-fn display_start_linear_address(_record: &StartLinearAddressRecord, ui: &mut Ui) {
-    ui.label("Start linear address {NOT IMPLEMENTED YET}");
+fn display_start_linear_address(record: &StartLinearAddressRecord, ui: &mut Ui) {
+    ui.label(format!("Start linear address: 0x{:x}", record.entry_point));
 }
 
 fn display_record(i: usize, record: &IHexRecord, ui: &mut Ui) {
